1254b0968c543ff66fc08cf8ee628c6c0124a4cc
ValentinaPanic <valentina.panic87@gmail.com>
2021-01-26 20:59:47 -0700

    commit tree 89437d3880942be91476a045986294a37d0bb039

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,15 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({display_title, summary_short}) => {
+    return(
+        <ul className="review-list">
+            <li className="review">
+                {display_title}
+            </li>
+          <p>{summary_short}</p> 
+        </ul>
+
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,28 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            movies: [
+                // searchTerm: " ",
+                // reviews: " "
+            ]
+        }
+    }
+    // componentDidMount(){
+    //     fetch(URL)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+    // }
+
+    render(){
+        return(
+            <div>
+                hi
+            </div>
+        )
+    }
+}
